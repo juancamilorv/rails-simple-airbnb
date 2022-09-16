100bf8464266cfb4417c3a23622218f0bc7c1e8b
Juan Camilo Ramirez Velez <juancrv28@gmail.com>
2022-09-16 12:07:20 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+5.times do
+  flat = Flat.create!(name: Faker::Lorem.word,
+                      address: Faker::Address.street_address,
+                      description: Faker::Lorem.paragraph,
+                      price_per_night: rand(20..100),
+                      number_of_guests: rand(1..8))
+  puts "Restaurant #{flat.name} created!"
+end
